df5e7155dc9277ffbf8c1ab83f851b44130415f1
larevolucia <reis.lucia@gmail.com>
2024-07-28 16:00:00 +0200

    Changed from local spreadsheet to Google Sheets / Edited README

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # househunting
- Web Scrapping for properties that match buyers criteria
+
+Web scraping script checks Pararius.nl page for new entries using selenium.
+With panda, the data found is managed and inserted in google sheets.
+
+## Requires Google Sheets API configuration
+
+### Step 1: Go to Google Cloud Console
+
+    Open your web browser and go to Google Cloud Console.
+    If you’re not already signed in, sign in with your Google account.
+
+### Step 2: Select Your Project
+
+    If you don't have a project, quickly create one by clicking on New Project and providing a Project Name.
+    At the top left corner, click on the project dropdown.
+    Select your existing project from the list. If you don’t have a project, you need to create one by clicking "New Project".
+
+### Step 3: Open the Service Accounts Page
+
+    In the left-hand navigation menu, go to IAM & Admin > Service Accounts.
+
+### Step 4: Create a Service Account
+
+    Click on the "Create Service Account" button at the top.
+    Fill in the service account details:
+    Service account name: Enter a name for the service account.
+    Service account ID: This will be automatically filled based on the name.
+    Service account description: Enter a description (optional).
+    Click "Create and Continue".
+
+### Step 5: Grant the Service Account Access to the Project
+
+    In the "Grant this service account access to the project" section, choose a role that grants appropriate access for your needs. For example, if you are accessing Google Sheets, you might need the "Editor" role.
+    Click "Continue".
+
+### Step 6: Create Key for the Service Account
+
+    In the "Grant users access to this service account" section, click "Done".
+    You will be redirected back to the Service Accounts page. Find the service account you just created.
+    Click on the three dots on the right of the service account row, and select "Manage keys".
+    Click "Add Key" > "Create new key".
+    In the pop-up, select "JSON" and then click "Create".
+    A JSON file containing your service account key will be downloaded to your computer. This is the file you need for authentication.
+
+## Share Google Sheet with your Project
+
+Go to your google sheet and click on share. Share it with the client_email of your project.
--- main.py
+++ main.py
@@ -1,27 +1,64 @@
+"""This script aims to scrap properties listed at 
+ Pararius.nl according to specific parameters"""
+
 from datetime import datetime
-import pprint 
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import gspread
+from google.oauth2.service_account import Credentials
 import pandas as pd
 
-# URL for the properties listing in Amsterdam
-PARARIUS_URL = "https://www.pararius.nl/koopwoningen/amsterdam/0-500000/2-slaapkamers/50m2/sinds-3"
+# Load environment variables from .env file
+load_dotenv()
 
-# Path to the Excel file
-EXCEL_FILE = "C:/Users/reisl/Desktop/tmp/properties_listings.xlsx"
+# Load environment variables
+SPREADSHEET_ID = os.environ.get("SPREADSHEET_ID")
+GOOGLE_SHEETS_CREDENTIALS_PATH = os.getenv("GOOGLE_SHEETS_CREDENTIALS_PATH")
+SERVICE_ACCOUNT_FILE = (
+    GOOGLE_SHEETS_CREDENTIALS_PATH
+    if GOOGLE_SHEETS_CREDENTIALS_PATH
+    else "credentials.json"
+)
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SHEET_NAME = "Listings"
+
+
+# Authenticate and initialize the Google Sheets client
+creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+client = gspread.authorize(creds)
+
+# Attempt to access the specified spreadsheet and sheet
+try:
+    sheet = client.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
+except gspread.SpreadsheetNotFound:
+    print("Error: Spreadsheet not found. Please check the SPREADSHEET_ID.")
+    exit()
+except gspread.WorksheetNotFound:
+    print("Error: Worksheet not found. Please check the SHEET_NAME.")
+    exit()
+
+
+# URL for the properties listing in Amsterdam
+PARARIUS_URL = (
+    "https://www.pararius.nl/koopwoningen/amsterdam/0-500000/2-slaapkamers/50m2/sinds-3"
+)
 
 # Initialize the WebDriver for Firefox (ensure geckodriver is installed)
-driver = webdriver.Firefox()  # or webdriver.Chrome(), etc.
+driver = webdriver.Firefox()
 driver.get(PARARIUS_URL)
 
 # Wait until the properties are loaded
 try:
     WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[class='page__row page__row--search-list']"))
+        EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, "div[class='page__row page__row--search-list']")
+        )
     )
-except Exception as e:
+except Exception as e:  # pylint: disable=broad-exception-caught
     print("Error loading properties:", e)
     driver.quit()
     exit()
@@ -30,13 +67,23 @@ except Exception as e:
 listed_properties = {}
 
 # Find all property items on the page
-property_items = driver.find_elements(By.CSS_SELECTOR, "li[class='search-list__item search-list__item--listing']")
+property_items = driver.find_elements(
+    By.CSS_SELECTOR, "li[class='search-list__item search-list__item--listing']"
+)
 
 # Iterate over each property item and extract the required details
 for index, item in enumerate(property_items, start=1):
-    address = item.find_element(By.CSS_SELECTOR, "a[class='listing-search-item__link listing-search-item__link--title']").text.strip()
-    url_suffix = item.find_element(By.CSS_SELECTOR, "a[class='listing-search-item__link listing-search-item__link--depiction']").get_attribute("href")
-    price = item.find_element(By.CSS_SELECTOR, "div[class='listing-search-item__price']").text.strip()
+    address = item.find_element(
+        By.CSS_SELECTOR,
+        "a[class='listing-search-item__link listing-search-item__link--title']",
+    ).text.strip()
+    url_suffix = item.find_element(
+        By.CSS_SELECTOR,
+        "a[class='listing-search-item__link listing-search-item__link--depiction']",
+    ).get_attribute("href")
+    price = item.find_element(
+        By.CSS_SELECTOR, "div[class='listing-search-item__price']"
+    ).text.strip()
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     listed_properties[index] = {
@@ -46,27 +93,33 @@ for index, item in enumerate(property_items, start=1):
         "timestamp": timestamp,
     }
 
-pprint.pprint(listed_properties)
+# Convert the listed_properties dictionary to a DataFrame
+new_listings_df = pd.DataFrame.from_dict(listed_properties, orient="index")
+
+# Load the existing listings from the Google Sheet
+existing_listings = sheet.get_all_records()
+existing_listings_df = pd.DataFrame(existing_listings)
+
+# Check for new listings
+new_listings = new_listings_df[
+    ~new_listings_df["URL"].isin(existing_listings_df["URL"])
+]
+
+# Append new listings to the existing DataFrame and update the Google Sheet
+if not new_listings.empty:
+    updated_listings_df = pd.concat(
+        [existing_listings_df, new_listings], ignore_index=True
+    )
+    # Fill NaN values with an empty string
+    updated_listings_df = updated_listings_df.fillna("")
+    # Update the Google Sheet
+    sheet.update(
+        [updated_listings_df.columns.values.tolist()]
+        + updated_listings_df.values.tolist()
+    )
+    print(f"Added {len(new_listings)} new listings to the Google Sheet.")
+else:
+    print("No new listings found.")
 
 # Close the WebDriver
 driver.quit()
-
-# Convert the listed_properties dictionary to a DataFrame
-new_listings_df = pd.DataFrame.from_dict(listed_properties, orient='index')
-
-# Load the existing listings from the Excel file
-try:
-    existing_listings_df = pd.read_excel(EXCEL_FILE)
-except FileNotFoundError:
-    existing_listings_df = pd.DataFrame(columns=["address", "postal_code", "URL", "price", "timestamp"])
-
-# Check for new listings
-new_listings = new_listings_df[~new_listings_df["URL"].isin(existing_listings_df["URL"])]
-
-# Append new listings to the existing DataFrame
-if not new_listings.empty:
-    updated_listings_df = pd.concat([existing_listings_df, new_listings], ignore_index=True)
-    updated_listings_df.to_excel(EXCEL_FILE, index=False)
-    print(f"Added {len(new_listings)} new listings to the Excel file.")
-else:
-    print("No new listings found.")
